7ceb6d7580739200407f7958c1324e7178d0b7a1
Tyler <tylrdvs277@gmail.com>
2017-09-29 09:39:31 -0700

    29 September 2017

---
+++ queues.py
@@ -0,0 +1,111 @@
+# Name:        Tyler Davis
+# Course:      CPE 202
+# Instructor:  Dave Parkinson
+# Assignment:  Lab 3
+# Term:        Fall 2017
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, data):
+        self.data = data
+
+    def set_next(self, next):
+        self.next = next
+
+class QueueLinked:
+
+    def __init__(self, capacity):
+        # Creates an empty queue with a capacity
+        self.capacity = capacity
+        self.num_items = 0
+        self.front = None
+        self.end = None
+
+    def is_empty(self):
+        # Returns true if the queue is empty and false otherwise
+        return (self.num_items == 0)
+
+    def is_full(self):
+        # Returns true if the queue is full and false otherwise
+        return (self.num_items == self.capacity)
+
+    def num_in_queue(self):
+        # Returns the size of the queue
+        return self.num_items
+
+    def enqueue(self, item):
+        # Adds an item to the end of the queue if not full 
+        # and raises an error otherwise
+        if self.is_full():
+            raise IndexError
+        elif self.is_empty():
+            self.front = self.end = Node(item)
+        else:
+            temp = Node(item)
+            self.end.set_next(temp)
+            self.end = temp
+        self.num_items += 1
+
+    def dequeue(self):
+        # Removes and returns item from the front of the queue if not empty
+        # and raises an error otherwise
+        if self.is_empty():
+            raise IndexError
+        else:
+            data = self.front.get_data()
+            self.front = self.front.get_next()
+            self.num_items -= 1
+            return data
+
+
+class QueueArray:
+
+    def __init__(self, capacity):
+        # Creates an empty queue with a capacity
+        self.capacity = capacity
+        self.num_items = self.enqueue_idx = self.dequeue_idx = 0
+        self.items = [None] * capacity
+
+    def is_empty(self):
+        # Returns true if the queue is empty and false otherwise
+        return (self.num_items == 0)
+
+    def is_full(self):
+        # Returns true if the queue is full and false otherwise
+        return (self.num_items == self.capacity)
+
+    def num_in_queue(self):
+        # Returns size of the queue
+        return self.num_items
+
+    def enqueue(self, item):
+        # Adds an item to the end of the queue if not full 
+        # and raises an error otherwise
+        if self.is_full():
+            raise IndexError
+        else:
+            self.items[self.enqueue_idx] = item
+            self.enqueue_idx = (self.enqueue_idx + 1) % self.capacity
+            self.num_items += 1
+
+    def dequeue(self):
+        # Removes and returns item from the front of the queue if not empty
+        # and raises an error otherwise
+        if self.is_empty():
+            raise IndexError
+        else:
+            temp = self.items[self.dequeue_idx]
+            self.items[self.dequeue_idx] = None
+            self.dequeue_idx = (self.dequeue_idx + 1) % self.capacity
+            self.num_items -= 1
+            return temp
---
+++ test_queues.py
@@ -0,0 +1,139 @@
+# Name:        Tyler Davis
+# Course:      CPE 202
+# Instructor:  Dave Parkinson
+# Assignment:  Lab 3
+# Term:        Fall 2017
+
+import unittest
+import queues
+
+class TestLab3(unittest.TestCase):
+
+
+    def test_is_empty_1(self):
+        # Creates an empty queue and makes sure the function returns true
+        queue = queues.QueueLinked(10)
+        self.assertTrue(queue.is_empty())
+
+    def test_is_empty_2(self):
+        # Creates a queue and then adds an item, makes sure it isn't empty
+        queue = queues.QueueLinked(10)
+        queue.enqueue(1)
+        self.assertFalse(queue.is_empty())
+
+    def test_is_full_1(self):
+        # Creates a queue and fills it, makes sure its full
+        queue = queues.QueueLinked(5)
+        for i in range(5):
+            queue.enqueue(i)
+        self.assertTrue(queue.is_full())
+
+    def test_is_full_2(self):
+        # Creates an queue and makes sure it isn't full
+        queue = queues.QueueLinked(10)
+        self.assertFalse(queue.is_full())
+
+    def test_num_in_queue_1(self):
+        # Creates a queue and adds 4 things, makes sure size is four
+        queue = queues.QueueLinked(5)
+        for i in range(4):
+            queue.enqueue(i)
+        self.assertEqual(queue.num_in_queue(), 4)
+
+    def test_enqueue_1(self):
+        # Creates a queue, adds one thing, checks size, adds another, checks again
+        queue = queues.QueueLinked(10)
+        queue.enqueue(1)
+        self.assertEqual(queue.num_in_queue(), 1)
+        queue.enqueue(2)
+        self.assertEqual(queue.num_in_queue(), 2)
+
+    def test_enqueue_2(self):
+        # Creates a queue and fills it, makes sure that the next enqueue raises an error
+        queue = queues.QueueLinked(10)
+        for i in range(10):
+            queue.enqueue(i)
+        with self.assertRaises(IndexError):
+            queue.enqueue(1)
+
+    def test_dequeue_1(self):
+        # Creates a queue, adds two things, makes sure they are removed correctly 
+        queue = queues.QueueLinked(10)
+        queue.enqueue(1)
+        queue.enqueue(2)
+        self.assertEqual(queue.dequeue(), 1)
+        self.assertEqual(queue.dequeue(), 2)
+
+    def test_dequeue_2(self):
+        # Tries to remove from an empty queue, makes sure an error is raised
+        queue = queues.QueueLinked(10)
+        with self.assertRaises(IndexError):
+            queue.dequeue()
+
+
+    # The above tests are identical to the next tests except they check a different implementation
+
+
+    def test_array_is_empty_1(self):
+        # Creates an empty queue and makes sure the function returns true
+        queue = queues.QueueArray(10)
+        self.assertTrue(queue.is_empty())
+
+    def test_array_is_empty_2(self):
+        # Creates a queue and then adds an item, makes sure it isn't empty
+        queue = queues.QueueArray(10)
+        queue.enqueue(1)
+        self.assertFalse(queue.is_empty())
+
+    def test_array_is_full_1(self):
+        # Creates a queue and fills it, makes sure its full
+        queue = queues.QueueArray(5)
+        for i in range(5):
+            queue.enqueue(i)
+        self.assertTrue(queue.is_full())
+
+    def test_array_is_full_2(self):
+        # Creates an queue and makes sure it isn't full
+        queue = queues.QueueArray(10)
+        self.assertFalse(queue.is_full())
+
+    def test_array_num_in_queue_1(self):
+        # Creates a queue and adds 4 things, makes sure size is four
+        queue = queues.QueueArray(5)
+        for i in range(4):
+            queue.enqueue(i)
+        self.assertEqual(queue.num_in_queue(), 4)
+
+    def test_array_enqueue_1(self):
+        # Creates a queue, adds one thing, checks size, adds another, checks again
+        queue = queues.QueueArray(10)
+        queue.enqueue(1)
+        self.assertEqual(queue.num_in_queue(), 1)
+        queue.enqueue(2)
+        self.assertEqual(queue.num_in_queue(), 2)
+
+    def test_array_enqueue_2(self):
+        # Creates a queue and fills it, makes sure that the next enqueue raises an error
+        queue = queues.QueueArray(10)
+        for i in range(10):
+            queue.enqueue(i)
+        with self.assertRaises(IndexError):
+            queue.enqueue(1)
+
+    def test_array_dequeue_1(self):
+        # Creates a queue, adds two things, makes sure they are removed correctly 
+        queue = queues.QueueArray(10)
+        queue.enqueue(1)
+        queue.enqueue(2)
+        self.assertEqual(queue.dequeue(), 1)
+        self.assertEqual(queue.dequeue(), 2)
+
+    def test_array_dequeue_2(self):
+        # Tries to remove from an empty queue, makes sure an error is raised
+        queue = queues.QueueArray(10)
+        with self.assertRaises(IndexError):
+            queue.dequeue()
+
+
+if __name__ == "__main__":
+        unittest.main()
